da21449a353d7d396624208a1d101dfd8bd1a93a
Helen <hd411@nyu.edu>
2024-04-11 15:29:01 -0400

    Create new repo

---
+++ src/main/java/com/helendong/PantryPanda/model/User.java
@@ -0,0 +1,28 @@
+package com.helendong.PantryPanda.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+})
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    public User() {
+
+    }
+}
---
+++ src/test/java/com/helendong/PantryPanda/PantryPandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helendong.PantryPanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PantryPandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
